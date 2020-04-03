08eb666b4e0439bd28a9d8ac88aa12d5970f44c4
Kacper Kozak <kacper.kozak@siili.com>
2020-04-03 16:01:37 +0200

    Updates

---
+++ precache-manifest.8d1a77922a2778cb565bb6d817268557.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9522a1315753090a93360079e641e920",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b162c8c62e46a5655c7e",
+    "url": "/static/css/main.d26c0e68.chunk.css"
+  },
+  {
+    "revision": "2f8acf2361f8bc3bdcbf",
+    "url": "/static/js/2.67dd408b.chunk.js"
+  },
+  {
+    "revision": "767f72398b49443fb14778aa5d03df54",
+    "url": "/static/js/2.67dd408b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b162c8c62e46a5655c7e",
+    "url": "/static/js/main.dc1a39f6.chunk.js"
+  },
+  {
+    "revision": "691e20fb572e293c9974",
+    "url": "/static/js/runtime-main.e7707ae3.js"
+  }
+]);
