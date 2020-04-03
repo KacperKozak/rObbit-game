27c023084ce3f6071a9d678cf6d14cffbc703876
Kacper Kozak <kacper.kozak@siili.com>
2020-04-03 16:06:15 +0200

    Updates

---
+++ precache-manifest.31d7f700bb9d3d92af9c8557f1dcb29f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af9ee6864474212f54a948e84e53986",
+    "url": "/alpha-mechanical/index.html"
+  },
+  {
+    "revision": "b162c8c62e46a5655c7e",
+    "url": "/alpha-mechanical/static/css/main.d26c0e68.chunk.css"
+  },
+  {
+    "revision": "2f8acf2361f8bc3bdcbf",
+    "url": "/alpha-mechanical/static/js/2.67dd408b.chunk.js"
+  },
+  {
+    "revision": "767f72398b49443fb14778aa5d03df54",
+    "url": "/alpha-mechanical/static/js/2.67dd408b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b162c8c62e46a5655c7e",
+    "url": "/alpha-mechanical/static/js/main.dc1a39f6.chunk.js"
+  },
+  {
+    "revision": "0ccceb8c98682c4ea16b",
+    "url": "/alpha-mechanical/static/js/runtime-main.0fb3ba15.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d1a77922a2778cb565bb6d817268557.js"
+  "/alpha-mechanical/precache-manifest.31d7f700bb9d3d92af9c8557f1dcb29f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alpha-mechanical/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
