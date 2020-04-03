492270da12abfa51cd0197bc9c055af0e8449955
Kozak Bartosz <bartoszkozak94@gmail.com>
2020-04-03 19:51:04 +0200

    marge

--- src/mocks/mapMock.ts
+++ src/mocks/mapMock.ts
@@ -47,7 +47,7 @@ export const createMap = (): ObjectInstance[] => {
                 type,
                 xy: [x, y],
                 id: tileTypeDefinitions[type]!.getId(),
-                elevation: (type == ObjectTypes.Rock ? 1 : 0) + Math.random() / 7,
+                elevation: Math.random() / 7,
                 rotation: [0, 0],
                 zIndex: 1,
                 aIndex: 100,
