85fffcf99c08dc01f34853fc0360914c0d6e4fc8
Kacper Kozak <kacperkk@gmail.com>
2020-04-03 18:31:12 +0200

    Fix canvas positon

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,23 +6,7 @@ import { GameInstance } from './GameInstance'
 export const App = () => {
     return (
         <Provider store={store}>
-            <div>
-                <h1
-                    style={{
-                        position: 'fixed',
-                        top: 0,
-                        left: 0,
-                        right: 0,
-                        width: '300px',
-                        background: 'rgba(0,0,0,0.2)',
-                        margin: '0 auto',
-                        borderRadius: '0 0 15px 15px',
-                    }}
-                >
-                    alpha-mechanical
-                </h1>
-                <GameInstance />
-            </div>
+            <GameInstance />
         </Provider>
     )
 }
