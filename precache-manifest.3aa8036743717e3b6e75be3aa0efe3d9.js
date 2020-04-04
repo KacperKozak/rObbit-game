6e6d87de3e0aa326078f1c81ccc401d48c96e64a
Kacper Kozak <kacperkk@gmail.com>
2020-04-04 19:35:26 +0200

    Updates

--- precache-manifest.3aa8036743717e3b6e75be3aa0efe3d9.js
+++ precache-manifest.3aa8036743717e3b6e75be3aa0efe3d9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ae69afff7a9358298d4e42ae81606a9b",
+    "revision": "d421efe72229304a429b2612e2d52800",
     "url": "/alpha-mechanical/index.html"
   },
   {
-    "revision": "fffe5001425bb5970e21",
+    "revision": "d19636899d7f9d926b0d",
     "url": "/alpha-mechanical/static/css/main.dd83b899.chunk.css"
   },
   {
-    "revision": "2d58296586943257cfc1",
-    "url": "/alpha-mechanical/static/js/2.7de5cb05.chunk.js"
+    "revision": "596882e60796aa0cad5b",
+    "url": "/alpha-mechanical/static/js/2.bcb3dfd9.chunk.js"
   },
   {
     "revision": "767f72398b49443fb14778aa5d03df54",
-    "url": "/alpha-mechanical/static/js/2.7de5cb05.chunk.js.LICENSE.txt"
+    "url": "/alpha-mechanical/static/js/2.bcb3dfd9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fffe5001425bb5970e21",
-    "url": "/alpha-mechanical/static/js/main.d0e886da.chunk.js"
+    "revision": "d19636899d7f9d926b0d",
+    "url": "/alpha-mechanical/static/js/main.f11fa89c.chunk.js"
   },
   {
     "revision": "0ccceb8c98682c4ea16b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/alpha-mechanical/precache-manifest.b6bdb3ea68a3fde2d89a9254e7ccf76a.js"
+  "/alpha-mechanical/precache-manifest.3aa8036743717e3b6e75be3aa0efe3d9.js"
 );
 
 self.addEventListener('message', (event) => {
