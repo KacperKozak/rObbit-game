d57fac33ea896cf530fa8daf3b69243e430a3310
Kozak Bartosz <bartoszkozak94@gmail.com>
2020-04-05 22:18:53 +0200

    images

--- src/data/maps.ts
+++ src/data/maps.ts
@@ -13,13 +13,19 @@ import level4 from './level4.json'
 import level4img from './images/4.png'
 
 import level5 from './level5.json'
+import level5img from './images/5.png'
 
 import level6 from './level6.json'
+import level6img from './images/6.png'
 
 import level7 from './level7.json'
+import level7img from './images/7.png'
 
 import level8 from './level8.json'
+import level8img from './images/8.png'
+
 import level9 from './level9.json'
+import level9img from './images/9.png'
 
 import map0 from './map0.json'
 import map1 from './map1.json'
@@ -31,11 +37,11 @@ export const maps: MapData[] = [
     { id: 'level2', name: 'level2', image: level2img, objects: level2 as any },
     { id: 'level3', name: 'level3', image: level3img, objects: level3 as any },
     { id: 'level4', name: 'level4', image: level4img, objects: level4 as any },
-    { id: 'level5', name: 'level5', objects: level5 as any },
-    { id: 'level6', name: 'level6', objects: level6 as any },
-    { id: 'level7', name: 'level7', objects: level7 as any },
-    { id: 'level8', name: 'level8', objects: level8 as any },
-    { id: 'level9', name: 'level9', objects: level9 as any },
+    { id: 'level5', name: 'level5', image: level5img, objects: level5 as any },
+    { id: 'level6', name: 'level6', image: level6img, objects: level6 as any },
+    { id: 'level7', name: 'level7', image: level7img, objects: level7 as any },
+    { id: 'level8', name: 'level8', image: level8img, objects: level8 as any },
+    { id: 'level9', name: 'level9', image: level9img, objects: level9 as any },
     { id: 'map-0', name: 'Test 0', objects: map0 as any },
     { id: 'map-1', name: 'Test 1', objects: map1 as any },
     { id: 'map-2', name: 'Test 2', objects: map2 as any },
