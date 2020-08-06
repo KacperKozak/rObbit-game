5f83acb11a91cf85dbebeedfa4ac7d7ac98eefd7
Kacper Kozak <kacperkk@gmail.com>
2020-08-06 19:10:44 +0200

    Updates

---
+++ precache-manifest.92fc15327ccd2fed5c96482ca61025c3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fff748d08967f1018bb3facd2ab5760",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1e90cf28a9ef009139d",
+    "url": "/static/css/main.2e3341d1.chunk.css"
+  },
+  {
+    "revision": "78a2ede081887fd3d004",
+    "url": "/static/js/2.345805c5.chunk.js"
+  },
+  {
+    "revision": "e31e09f2f2ab566eef9237534c3c099a",
+    "url": "/static/js/2.345805c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1e90cf28a9ef009139d",
+    "url": "/static/js/main.d19160d9.chunk.js"
+  },
+  {
+    "revision": "bed2bce8ffeccfec506d",
+    "url": "/static/js/runtime-main.1dc34410.js"
+  },
+  {
+    "revision": "9359bbf7a3bf2c545bc6023aee62ce97",
+    "url": "/static/media/box.9359bbf7.png"
+  },
+  {
+    "revision": "e8ecee01390f193ab182ae08636e4281",
+    "url": "/static/media/button.e8ecee01.png"
+  },
+  {
+    "revision": "f203581684d55ef78ef09b89acef386f",
+    "url": "/static/media/cannon.f2035816.png"
+  },
+  {
+    "revision": "1726626a5b721020d601e84511989c40",
+    "url": "/static/media/cannonRecoil.1726626a.png"
+  },
+  {
+    "revision": "813aa778369d02bdc9299dd6ad2caffd",
+    "url": "/static/media/cliff.813aa778.png"
+  },
+  {
+    "revision": "f5e329aba9ae930045c0a52291d81a67",
+    "url": "/static/media/columns.f5e329ab.png"
+  },
+  {
+    "revision": "349b8cd56ced5005322efaa8d6da0c49",
+    "url": "/static/media/crossbow.349b8cd5.png"
+  },
+  {
+    "revision": "8b2e35561b218ab415a99f485f8b5cd0",
+    "url": "/static/media/dustPipe.8b2e3556.png"
+  },
+  {
+    "revision": "3bf1291714f5dcc8e439b6856fdd277a",
+    "url": "/static/media/dustRiver.3bf12917.png"
+  },
+  {
+    "revision": "1c236bcc45f16118f5598ff6d82b0c23",
+    "url": "/static/media/greenRiver.1c236bcc.png"
+  },
+  {
+    "revision": "2e7a2e65076b28e5beac0535305c2281",
+    "url": "/static/media/iceworld.2e7a2e65.png"
+  },
+  {
+    "revision": "cc1ac8769cf648710b7fe3a4cd36546c",
+    "url": "/static/media/intro.cc1ac876.mp4"
+  },
+  {
+    "revision": "99f2de958d1407f47ba274f8a5561ca0",
+    "url": "/static/media/lake.99f2de95.png"
+  },
+  {
+    "revision": "83dc5a64b3ec321ef0628da997492d03",
+    "url": "/static/media/mountains.83dc5a64.png"
+  },
+  {
+    "revision": "9557d880ed25ad5bc6677f08797c9332",
+    "url": "/static/media/outro.9557d880.mp4"
+  },
+  {
+    "revision": "616950645c3da2baeeb1dcf84feeadc2",
+    "url": "/static/media/stairs.61695064.png"
+  },
+  {
+    "revision": "6dc8f9ca6b55ae66f0825ab44784257b",
+    "url": "/static/media/walk.6dc8f9ca.png"
+  },
+  {
+    "revision": "2bbbafe6436612f666755f9e686e10fb",
+    "url": "/static/media/wall.2bbbafe6.png"
+  }
+]);
